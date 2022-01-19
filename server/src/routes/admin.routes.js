5d5bd94d42e5a8093e166c78d998804a7d61516d
Redouane Bouabana <rredouane342@gmail.com>
2022-01-19 16:48:59 +0100

    cleanup

--- server/src/routes/admin.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const admin = express.Router()
-const { loginAdmin  } = require('../controllers/admin.controller')
-
-admin.post('/login', loginAdmin);
-module.exports = admin
