248823c92f629f76f04198900d29ce33bc2e805a
Redouane Bouabana <rredouane342@gmail.com>
2022-01-22 01:56:35 +0100

    init react app

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <p className="bg-slate-900 h-screen">
+        <h1 className="text-[22em] font-extrabold text-transparent antialiased bg-clip-text bg-gradient-to-tr from-blue-400 to-red-600 text-center m-auto ">
+        yep
+        </h1>
+      </p>
+    </div>
+  );
+}
+
+export default App;
