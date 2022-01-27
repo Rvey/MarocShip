26a4e3428966bdd1a131543959168747bf18eb4a
Redouane Bouabana <rredouane342@gmail.com>
2022-01-27 01:21:34 +0100

    persist data in redux store

--- client/src/Redux/store.ts
+++ client/src/Redux/store.ts
@@ -1,15 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './features/counter/counterSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { managerApi } from './services/managers';
 import userReducer from './features/auth/userSlice';
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-        user: userReducer,
+        user: persistedReducer,
         [managerApi.reducerPath]: managerApi.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(managerApi.middleware)
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+            }
+        }).concat(managerApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,13 +6,18 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 import { CookiesProvider } from 'react-cookie';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+let persistor = persistStore(store);
 ReactDOM.render(
     <Provider store={store}>
-        <React.StrictMode>
-            <CookiesProvider>
-            <App />
-            </CookiesProvider>
-        </React.StrictMode>
+        <PersistGate loading={null} persistor={persistor}>
+            <React.StrictMode>
+                <CookiesProvider>
+                    <App />
+                </CookiesProvider>
+            </React.StrictMode>
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
